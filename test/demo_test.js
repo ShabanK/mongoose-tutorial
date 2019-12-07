f6e3ffe3bf31f9668d4184e2cad500a80c04c9b4
ShabanK <nooneyouwillknow@gmail.com>
2019-12-07 11:02:25 +0500

    saved a record

--- test/demo_test.js
+++
@@ -1,9 +0,0 @@
-const assert = require("assert");
-
-//describing tests
-
-describe("some demo tests", () => {
-  it("adds two numbers together", () => {
-    assert(2 + 3 === 5);
-  });
-});
---
+++ test/savingTests.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const MarioChar = require("../models/mariochar");
+//describing tests
+
+describe("Saving Records", () => {
+  it("Saves a record to a database", done => {
+    let char = new MarioChar({ name: "Peach" });
+    char.save().then(() => {
+      assert(char.isNew === false);
+      done();
+    });
+  });
+});
