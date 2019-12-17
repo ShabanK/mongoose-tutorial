8f9613e9d7c988c5608e372a7eecae8a7fd0ad0c
unknown <shabankhawar99@gmail.com>
2019-12-17 23:39:25 +0500

    wrote a test to serach by ObjectID

--- test/findingTests.js
+++ test/findingTests.js
@@ -2,10 +2,10 @@ const assert = require("assert");
 const MarioChar = require("../models/mariochar");
 
 //describing tests
-
+var char;
 describe("Finding Records", () => {
   beforeEach(done => {
-    let char = new MarioChar({ name: "YEET" });
+    char = new MarioChar({ name: "YEET" });
     char.save().then(() => {
       done();
     });
@@ -15,6 +15,14 @@ describe("Finding Records", () => {
     MarioChar.findOne({ name: "YEET" }).then(result => {
       console.log(result);
       assert(result.name === "YEET");
+      done();
+    });
+  });
+
+  it("Finds a record by Object ID in the database", done => {
+    MarioChar.findOne({ _id: char._id }).then(result => {
+      console.log(result);
+      assert(result._id.toString() === char._id.toString());
       done();
     });
   });
