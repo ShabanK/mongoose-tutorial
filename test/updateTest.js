21bbc6ddcbcc354c575e9a610300b88b37da7e4d
unknown <shabankhawar99@gmail.com>
2019-12-18 22:38:12 +0500

    created a new schema

--- test/updateTest.js
+++ test/updateTest.js
@@ -1,26 +1,26 @@
-const assert = require("assert");
-const MarioChar = require("../models/mariochar");
+// const assert = require("assert");
+// const MarioChar = require("../models/mariochar");
 
-//describing tests
-var char;
+// //describing tests
+// var char;
 
-beforeEach(done => {
-  char = new MarioChar({ name: "YEET" });
-  char.save().then(() => {
-    done();
-  });
-});
+// beforeEach(done => {
+//   char = new MarioChar({ name: "YEET" });
+//   char.save().then(() => {
+//     done();
+//   });
+// });
 
-describe("Updating records", () => {
-  it("Updates a record in the database", done => {
-    MarioChar.findOneAndUpdate({ name: "YEET" }, { name: "Sir YEET" }).then(
-      () => {
-        MarioChar.findOne({ name: "Sir YEET" }).then(result => {
-          console.log(result);
-          assert(true);
-          done();
-        });
-      }
-    );
-  });
-});
+// describe("Updating records", () => {
+//   it("Updates a record in the database", done => {
+//     MarioChar.findOneAndUpdate({ name: "YEET" }, { name: "Sir YEET" }).then(
+//       () => {
+//         MarioChar.findOne({ name: "Sir YEET" }).then(result => {
+//           console.log(result);
+//           assert(true);
+//           done();
+//         });
+//       }
+//     );
+//   });
+// });
