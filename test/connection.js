4907e476bf68ff97cb1604433e42326d00c16769
unknown <shabankhawar99@gmail.com>
2019-12-20 11:49:28 +0500

    final touches

--- test/connection.js
+++ test/connection.js
@@ -1,27 +1,27 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-before(done => {
-  mongoose.connect("mongodb://127.0.0.1/testdb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+// before(done => {
+//   mongoose.connect("mongodb://127.0.0.1/testdb", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
 
-  mongoose.connection
-    .once("open", () => {
-      console.log("You have connected to your database");
-      done();
-    })
-    .on("error", err => {
-      console.log(err);
-    });
-});
+//   mongoose.connection
+//     .once("open", () => {
+//       console.log("You have connected to your database");
+//       done();
+//     })
+//     .on("error", err => {
+//       console.log(err);
+//     });
+// });
 
-//dropping the collection before each test...
-beforeEach(done => {
-  mongoose.connection.collections.mariochars.drop(() => {
-    console.log("Dropped...");
-    done();
-  });
-});
+// //dropping the collection before each test...
+// beforeEach(done => {
+//   mongoose.connection.collections.mariochars.drop(() => {
+//     console.log("Dropped...");
+//     done();
+//   });
+// });
